0101fc4a82d6343abf9cde3937fcd8ca0fae8578
windanchaos <yaobo7878@sina.com>
2017-02-23 00:18:54 +0800

    新增input输入方法，旨在达到丰富输入框内容，增加图片选择随机等的差异。

---
+++ return_input.py
@@ -0,0 +1,58 @@
+import time, sys, random
+'''
+随机选择图片
+随机生成价格
+随机生成库存
+随机生产sku编码
+生成带时间戳的产品名称
+随机给范围数字
+'''
+
+
+def random_number_int(number):
+    """
+    随机给出范围内的整数,有random.randint(1,50)
+    :param number: 范围最大值
+    :return:
+    """
+    return int(random.random() * number)
+
+def random_number_float(number):
+    """
+    随机给出范围内的两位实数
+    :param number: 范围最大值
+    :return:
+    """
+    return round(random.random() * number, 2)
+
+def date_uni_string(your_str):
+    """
+    返回时间+string，构造具有时间差别的名称
+    """
+    return  time.strftime("%Y-%m-%d日%H:%M", time.localtime())+your_str
+
+def test_random_number_int():
+    print("random_number_int test====")
+    for i in range(1, 10):
+        print(random_number_int(5))
+
+def random_xpath(part1,random_number,part2):
+    """
+    返回随机的xpath或者其他类似的元素路径
+    :param part1: 路径前半部分
+    :param random_number: 随机数的最大范围
+    :param part2: 路径后半部分
+    :return: 全路径
+    """
+    return part1+str(random_number_int(random_number))+part2
+
+def test_random_number_float():
+    print("random_number_float test====")
+    for i in range(1, 10):
+        print(random_number_float(5))
+
+test_random_number_int()
+test_random_number_float()
+print(date_uni_string("测试商品"))
+print(random_xpath('//*[@id="addSkuWindow"]/div/div/fieldset[',12,']/div/div/span/span/input'))
+print(random.randint(1,50))
