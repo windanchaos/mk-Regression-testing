b83aadef8b3d1a68f48415ce557ea866427311f3
bojiang <bojiang@maike51.com>
2017-09-26 20:57:07 +0800

    update

--- appiumDemo/demo01.py
+++ appiumDemo/demo01.py
@@ -16,18 +16,21 @@ desired_caps['noReset'] = 'true'
 desired_caps['platformVersion'] = '4.4.4'
 desired_caps['platformName'] = 'Android'
 desired_caps['ChromeOptions.CAPABILITY'] = ["androidProcess", "com.tencent.mm:tools"]
-desired_caps['appWaitActivity'] = 'com.tencent.mm'
+
 
 
 driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 #print(driver.getPageSource)
 driver.implicitly_wait(10)
-driver.findElementByXPath("//*[@text='我']").click()
-driver.findElementByXPath("//*[@text='收藏']").click()
-driver.findElementByXPath("//*[contains(@text, '美团外卖')]").click()
+driver.find_element_by_xpath("//*[@text='南街的小白楼']").click()
+driver.find_element_by_xpath("//*[@text='沙箱测试']").click()
+
+#driver.findElementByXPath("//*[@text='南街的小白楼']").click()
+#driver.findElementByXPath("//*[@text='收藏']").click()
+#driver.findElementByXPath("//*[contains(@text, '美团外卖')]").click()
 #print(driver.getPageSource)
 #print(driver.getContextHandles)
-driver.context("WEBVIEW_com.tencent.mm:toolRs")
+#driver.context("WEBVIEW_com.tencent.mm:toolRs")
 #print(driver.getPageSource)
 
 
